aff8cb5bc5e2b91b099dd3c1c19ccf29e9cddf81
Shammo <kaziwasifamin@hotmail.com>
2022-07-05 00:55:25 +0600

    new classes added and password, photo added

--- backend/administrator/src/main/java/coaching/administrator/classes/Religion/Religion.java
+++ backend/administrator/src/main/java/coaching/administrator/classes/Religion/Religion.java
@@ -2,6 +2,7 @@ package coaching.administrator.classes.Religion;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,16 +11,20 @@ import javax.persistence.Table;
 public class Religion {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
     public Religion() {
+        System.out.println("\033[31minside religion default constructor\033[0m" + this.name);
+
     }
 
     public Religion(Integer id, String name) {
         this.id = id;
         this.name = name;
+        System.out.println("\033[31minside religion  parameterizedconstructor\033[0m");
+
     }
 
     public Integer getId() {
